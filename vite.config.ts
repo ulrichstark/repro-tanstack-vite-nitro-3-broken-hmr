4b5e490dae4c2b6a6534d35cb90f511d21fc36c7
Ulrich Stark <github@ustark.de>
2026-02-04 08:54:22 +0100

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { nitro as nitroV3PLugin } from "nitro/vite";
+import { nitro as nitroV3Plugin } from "nitro/vite";
 import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { defineConfig } from "vite";
 
@@ -9,7 +9,7 @@ export default defineConfig(() => {
         plugins: [
             tanstackStart(),
             // nitroV2Plugin(), // v2
-            nitroV3PLugin(), // v3
+            nitroV3Plugin(), // v3
             react(),
         ],
         server: { port: 3000, open: true, host: true },
